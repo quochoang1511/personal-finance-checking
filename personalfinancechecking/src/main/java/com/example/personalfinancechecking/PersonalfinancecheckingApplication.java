69921cb49347555275c99eb1eb332708ce0a8edf
WIN-6BOU8GF8IFA\admin <quochoangnguyen2003ct@gmail.com>
2026-03-13 10:55:14 +0700

    add package init project

---
+++ personalfinancechecking/src/main/java/com/example/personalfinancechecking/PersonalfinancecheckingApplication.java
@@ -0,0 +1,13 @@
+package com.example.personalfinancechecking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalfinancecheckingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalfinancecheckingApplication.class, args);
+	}
+
+}
---
+++ personalfinancechecking/src/test/java/com/example/personalfinancechecking/PersonalfinancecheckingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.personalfinancechecking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalfinancecheckingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
