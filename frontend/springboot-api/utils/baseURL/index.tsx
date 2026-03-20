c6c2a1ce39b21670522f59f955e44f519f706e30
WIN-6BOU8GF8IFA\admin <quochoangnguyen2003ct@gmail.com>
2026-03-20 10:50:23 +0700

    add proxy url

--- frontend/springboot-api/utils/baseURL/index.tsx
+++ frontend/springboot-api/utils/baseURL/index.tsx
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({  
-//   baseURL: "https://catkinhonline.onrender.com/api/", // Sử dụng URL Của APIGateWay
-  baseURL: "http://finance-alb-1996393017.us-east-1.elb.amazonaws.com/api/v1/", // Sử dụng URL Của APIGateWay
-  //  baseURL: `http://192.168.219.88:8080/api/v1/`, // Sử dụng URL Của APIGateWay
-  // baseURL: "https://7nt1w10n-7057.asse.devtunnels.ms/api/", // Sử dụng URL Của APIGateWay
+  baseURL: "/api/v1/", // Sử dụng URL Của APIGateWay
+
 });
 export default request;
